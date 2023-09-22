3f3481e11d101dfd3b83d0d50827c181706278d2
Sourik Bhuiya <sourikbhuiya@gmail.com>
2023-09-22 10:51:01 +0530

    User schema updated

---
+++ models/User.js
@@ -0,0 +1,54 @@
+import { Schema, model } from 'mongoose'
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String
+    },
+    firstname: {
+        type: String
+    },
+    lastname: {
+        type: String
+    },
+    phone: {
+        type: Number,
+        unique: true,
+        sparse: true,
+        validate: {
+            validator: value => phoneRegex.test(value),
+            message: props => `${props.value} is not a valid 10-digit phone number!`
+        }
+    },
+    image: {
+        type: String
+    },
+    address: {
+        type: String
+    },
+    sex: {
+        type: String,
+        enum: ['Male', 'Female', 'None'],
+        default: 'None'
+    },
+    bloodGroup: {
+        type: String,
+        enum: ['O+', 'O-', 'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-']
+    },
+    monthsDue: {
+        type: Number,
+        default: 0
+    },
+    status: {
+        type: String,
+        enum: ['Active', 'Inactive'],
+        default: 'Active'
+    }
+})
+
+const User = model('User', userSchema)
+export default User
