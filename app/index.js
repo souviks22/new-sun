b11506263212fc61664a5833ad7deeac9530be2f
Sourik Bhuiya <sourikbhuiya@gmail.com>
2024-02-07 13:54:29 +0530

    Cors

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
+import cors from 'cors'
 import dotenv from "dotenv"
 import express from "express"
 import mongoose from "mongoose"
 import authRouter from "../routers/auth.router.js"
 import userRouter from "../routers/user.router.js"
-
 const app = express()
 dotenv.config()
-
+app.use(cors())
 mongoose.connect(process.env.DB_URL)
     .then(() => console.log('Database Connected'))
     .catch(() => console.log('Failed to Connect to Database'))
