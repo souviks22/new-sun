457a7deb441445e3f9c4637220fbffc3a7a72632
Souvik Sarkar <souviksarkar2k3@gmail.com>
2025-09-13 01:06:58 +0530

    Mail trial-n-error

--- utility/mailer.js
+++ utility/mailer.js
@@ -3,8 +3,9 @@ import nodemailer from "nodemailer"
 process.env.NODE_ENV !== 'production' && process.loadEnvFile()
 
 const transporter = nodemailer.createTransport({
-    host: process.env.MAIL_SERVICE_HOST,
-    port: process.env.MAIL_SERVICE_PORT,
+    service: 'gmail',
+    // host: process.env.MAIL_SERVICE_HOST,
+    // port: process.env.MAIL_SERVICE_PORT,
     secure: true,
     auth: {
         user: process.env.MAIL_ID,
